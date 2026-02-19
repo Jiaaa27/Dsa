6df938389d83ad4aa6b1b925febcd1e8a47fac86
Jiaaa27 <148185000+Jiaaa27@users.noreply.github.com>
2026-02-19 15:36:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs=require('fs');
+//const fs=require('fs');
 /*fs.writeFile("./a.txt","abes college is",() => { });
 fs.readFile("./a.txt","utf-8",(err,result) => { 
     if(err){
@@ -10,6 +10,23 @@ fs.readFile("./a.txt","utf-8",(err,result) => {
 });*/
 //fs.cpSync("./a.txt","./.txt");
 //fs.unlinkSync("./a.txt");
-const os=require('os');
+/*const os=require('os');
 console.log("platform",os.platform());
 console.log("userinfo,",os.userInfo());
+console.log("CPU",os.arch());
+console.log("free memory",os.freemem());
+console.log("total memory",os.totalmem());
+console.log("uptime",os.uptime());
+console.log("home dir",os.homedir());
+console.log("host name",os.hostname());*/
+
+const http=require('http');
+const fs=require('fs');
+
+const home=fs.readFileSync('./a.html');
+const myserver=http.createServer((req,res) => {
+    res.end(home);
+})
+myserver.listen(8000,() => {
+    console.log("server is running");
+})
